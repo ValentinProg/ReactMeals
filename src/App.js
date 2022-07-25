db05a5572565e809499a7e9156ccff58118f9568
Valentin <V.Kovalenko.prog@gmail.com>
2022-07-25 16:47:28 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Heder/Header";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
